8186363471da261821fe2c74dffb1c5bdc3fd5b9
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-30 06:38:28 +0600

    create authApi.ts f

---
+++ lib/authApi.ts
@@ -0,0 +1,29 @@
+import { axiosInstance } from "@/lib/axiosInstance";
+
+export const authApi = {
+    register: async (payload: {
+        name: string;
+        email: string;
+        password: string;
+    }) => {
+        const res = await axiosInstance.post("/api/v1/auth/register", payload);
+        return res.data;
+    },
+
+    login: async (payload: { email: string; password: string }) => {
+        const res = await axiosInstance.post("/api/v1/auth/login", payload);
+        return res.data;
+    },
+
+    logout: async () => {
+        const res = await axiosInstance.post("/api/v1/auth/logout");
+        return res.data;
+    },
+
+    getMe: async () => {
+        const res = await axiosInstance.get("/api/v1/auth/me");
+        return res.data;
+    },
+
+    
+};

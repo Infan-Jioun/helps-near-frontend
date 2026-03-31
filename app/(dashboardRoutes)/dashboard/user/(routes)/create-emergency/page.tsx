e02d4d0070390d1ae1c46b771d1d531a16f2f76b
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-31 12:43:24 +0600

    create AddEmergency

---
+++ app/(dashboardRoutes)/dashboard/user/(routes)/create-emergency/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CreateEmargency from '../../../shared/CreateEmargency'
+
+export default function AddEmergency() {
+    return (
+        <div><CreateEmargency /></div>
+    )
+}

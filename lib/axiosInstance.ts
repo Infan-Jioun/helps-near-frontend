e92be6f1dea90de0c0147ddde63377463a8d6b67
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-04-01 12:59:36 +0600

    axiso Interseptor use

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,9 +1,23 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://helps-near-backend-blond.vercel.app",
+    baseURL: typeof window !== "undefined"
+        ? "" 
+        : process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            try {
+                await axiosInstance.post("/api/v1/auth/refresh-token");
+                return axiosInstance(error.config);
+            } catch {
+                window.location.href = "/login";
+            }
+        }
+        return Promise.reject(error);
+    }
+);

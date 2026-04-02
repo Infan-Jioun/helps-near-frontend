575918d4a745e6592caaafabd17e2b705ee7322a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-04-02 22:49:46 +0600

    added foorter

--- app/(dashboardRoutes)/dashboard/user/volunteer-response/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Response from '../../shared/Response'
-
-export default function VolunteerResponse() {
-    return (
-        <div><Response></Response></div>
-    )
-}

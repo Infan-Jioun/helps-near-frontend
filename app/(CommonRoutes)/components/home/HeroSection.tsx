d9cd026784f15357bddd15d12c0d52d87712cc5f
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-05-18 06:20:02 +0600

    fix: simplify dashboard route logic in HeroSection component

--- app/(CommonRoutes)/components/home/HeroSection.tsx
+++ app/(CommonRoutes)/components/home/HeroSection.tsx
@@ -175,10 +175,7 @@ export default function HeroSection() {
         fetchUser()
     }, [])
 
-    const dashboardRoute =
-        user?.role === "ADMIN" ? "/dashboard/admin/create-emergency"
-            : user?.role === "VOLUNTEER" ? "/dashboard/volunteer/create-emergency"
-                : "/dashboard/user/create-emergency"
+ 
 
     return (
         <>
@@ -319,7 +316,7 @@ export default function HeroSection() {
                                     style={{ background: "#dc2626", color: "#fff", border: "none" }}
                                     asChild
                                 >
-                                    <Link href={dashboardRoute}>
+                                    <Link href={"/dashboard/user/create-emergency"}>
                                         <AlertTriangle className="w-4 h-4" strokeWidth={2.5} />
                                         Report Emergency Now
                                     </Link>
